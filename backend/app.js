b0a80cd7cc4b1ebcc32a7e417354d3538dbd4a52
dmitry.samoylenko <samdimgensanta@gmail.com>
2023-07-03 20:04:24 +0300

    add versel config

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');
 const userRouter = require('./routes/userRoute')
+const cors = require('cors')
 const app = express();
 
 
-
+app.use(cors(
+    {
+        origin: ["https://leaderboard-ivory.vercel.app/"],
+        methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH']
+    }
+))
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 })
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/leaderboard', userRouter);
 
 app.all('*', (req, res, next) => {
-  next(`can't find ${req.originalUrl} on this server !`);
+    next(`can't find ${req.originalUrl} on this server !`);
 });
 
 module.exports = app;
